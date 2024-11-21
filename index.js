df1c41d86df108d44df9a09b2007be2e09d0d301
MateusMacielrj <mateusmacielrj@gmail.com>
2024-11-21 20:01:59 -0300

    Add
    addd trabalho

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector(".alunos").onclick = function () {
+  window.location.href = "https://portal.uniasselvi.com.br/graduacao/alunos";
+};
+
+
+document.querySelector(".ambiente").onclick = function () {
+    window.location.href = "https://login.uniasselvi.com.br/";
+};
+
+document.querySelector(".bibliotecas").onclick = function () {
+   window.location.href = "https://portal.uniasselvi.com.br/graduacao/alunos/bibliotecas"; 
+};
